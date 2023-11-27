297d80457ef6f646fb4882060320467dd3b1fc56
kojiadrianojr <iisparkplugiiog@yahoo.com>
2023-11-27 15:12:01 +0800

    Hide bg on nft page

--- src/views/MarketplaceV2/components/Card/Display.tsx
+++ src/views/MarketplaceV2/components/Card/Display.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Flex } from '@metagg/mgg-uikit'
 import { GoogleDriveLink } from 'views/MarketplaceV2/constants/config'
 import { QueryType, useQueryAsset } from 'hooks/useMarketplaceV2'
 import { Display } from './styled'
 import SvgIcon from '../Foundation/SvgIcon'
+
 
 type Props = {
   spriteName: string
   width?: number
   height?: number
   style?: any
+  hideBg?: boolean
 }
 
-const SpriteDisplay = ({ spriteName, width, height, style }: Props) => {
+const SpriteDisplay = ({ spriteName, width, height, style, hideBg }: Props) => {
   const spriteId = useQueryAsset({
     name: spriteName,
     type: QueryType.SPRITES,
@@ -20,10 +23,14 @@ const SpriteDisplay = ({ spriteName, width, height, style }: Props) => {
   const h = height ?? 130
   const src = GoogleDriveLink + spriteId
   const Img = <img alt="logo" src={src} />
-  return (
+  return !hideBg ? (
     <Display bg={src} style={style}>
       <SvgIcon Img={Img} width={w} height={h} />
     </Display>
+  ) : (
+    <Flex alignItems='center' justifyContent='center' style={style}>
+      <SvgIcon Img={Img} width={w} height={h} />
+    </Flex>
   )
 }
 
@@ -33,4 +40,5 @@ SpriteDisplay.defaultProps = {
   width: 130,
   height: 130,
   style: {},
+  hideBg: false,
 }
