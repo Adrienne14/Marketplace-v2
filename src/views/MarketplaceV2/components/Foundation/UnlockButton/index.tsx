8d7a654fa913d1b802f3e3666cce712369ec35ad
Salvador Amba Jr <46850774+Buko-pie@users.noreply.github.com>
2023-12-28 17:45:36 +0800

    first init

---
+++ src/views/MarketplaceV2/components/Foundation/UnlockButton/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, useWalletModal } from '@metagg/mgg-uikit'
+import styled from 'styled-components'
+import useAuth from '../../../../../hooks/useAuth'
+import useMarketplaceV2 from '../../../../../hooks/useMarketplaceV2'
+import { useTranslation } from '../../../../../contexts/Localization'
+import { COLORS } from '../../../../../views/MarketplaceV2/styles/constants'
+
+const UnlockButton = (props: any) => {
+  const { customTitle } = props
+  const { t } = useTranslation()
+  const { login, logout } = useAuth()
+  const { onPresentConnectModal } = useWalletModal(login, logout)
+  const { controllers } = useMarketplaceV2()
+  const { modal } = controllers
+
+  const handleClick = () => {
+    modal.handleClose('auth')
+    onPresentConnectModal()
+  }
+
+  return (
+    <StyledConnect onClick={handleClick} {...props} fullWidth>
+      {!customTitle ? 'Connect Wallet' : customTitle}
+    </StyledConnect>
+  )
+}
+
+export default UnlockButton
+
+const StyledConnect = styled(Button)`
+  background-color: ${COLORS.MAIN};
+  border: 1px solid ${({ theme }) => theme.colors.text};
+`
